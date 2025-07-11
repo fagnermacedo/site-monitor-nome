a379adfd05bec247208c61e834441fed08735534
Fagner Sá <fagnermacedo@yahoo.com.br>
2025-07-11 00:02:03 -0300

    Incluir a coluna, nome do arquivo.

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ fetch("resultados.json")
   .then(data => {
     const tbody = document.querySelector("#tabela tbody");
     data.forEach(reg => {
+      // Extrai o nome do arquivo da URL
+      const url = reg.url;
+      const nomeArquivo = url.substring(url.lastIndexOf('/') + 1) || "(página)";
+
       const tr = document.createElement("tr");
       tr.innerHTML = `
         <td>${new Date(reg.data).toLocaleString()}</td>
-        <td><a href="${reg.url}" target="_blank">🔗 Link</a></td>
+        <td><a href="${url}" target="_blank">🔗 Link</a></td>
+        <td>${nomeArquivo}</td>  <!-- Coluna "Nome Arquivo" -->
         <td>${reg.trecho}</td>
       `;
       tbody.appendChild(tr);
