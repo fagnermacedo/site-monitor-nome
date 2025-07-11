f6111011dd4af7fd68dbec8d91ca26a613e7b8b3
Fagner Sá <fagnermacedo@yahoo.com.br>
2025-07-11 00:14:23 -0300

    Ajuste para mostrar textos com acentuação correta

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+function escapeHtml(text) {
+  return text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
 fetch("resultados.json")
   .then(res => res.json())
   .then(data => {
     const tbody = document.querySelector("#tabela tbody");
     data.forEach(reg => {
-      // Extrai o nome do arquivo da URL
       const url = reg.url;
       const nomeArquivo = url.substring(url.lastIndexOf('/') + 1) || "(página)";
-
+      
       const tr = document.createElement("tr");
       tr.innerHTML = `
         <td>${new Date(reg.data).toLocaleString()}</td>
         <td><a href="${url}" target="_blank">🔗 Link</a></td>
-        <td>${nomeArquivo}</td>  <!-- Coluna "Nome Arquivo" -->
-        <td>${reg.trecho}</td>
+        <td>${nomeArquivo}</td>
+        <td>${escapeHtml(reg.trecho)}</td>
       `;
       tbody.appendChild(tr);
     });
